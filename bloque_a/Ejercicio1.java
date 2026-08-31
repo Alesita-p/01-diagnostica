f59cfc1e5c17bf61f06e82b5d412e4baf56689e9
Alesita-p <alejandrapsos24@gmail.com>
2026-08-31 14:16:01 -0500

    Add variables and print student information

--- bloque_a/Ejercicio1.java
+++ bloque_a/Ejercicio1.java
@@ -1,8 +1,17 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
         // TODO: Declara aquí tus variables y asígnales valores
-
-        // TODO: Muestra los valores por consola
+        int edad = 17;
+        double notaFinal = 9.5;
+        char paralelo = 'A';
+        boolean matriculado = true;
+        String nombreEstudiante = "Alesita";
         
+        // TODO: Muestra los valores por consola
+        System.out.println("Estudiante: " + nombreEstudiante);
+        System.out.println("Edad: " + edad + " años");
+        System.out.println("Nota Final: " + notaFinal);
+        System.out.println("Paralelo: " + paralelo);
+        System.out.println("¿Está matriculado?: " + matriculado);
     }
 }
