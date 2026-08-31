88f4bb715aa21f8614daf22f845bdafb21862806
Alesita-p <alejandrapsos24@gmail.com>
2026-08-31 15:00:07 -0500

    Implement age input and evaluation logic

--- bloque_a/Ejercicio3.java
+++ bloque_a/Ejercicio3.java
@@ -2,9 +2,16 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        // TODO: Leer la edad con Scanner
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.print("Introduce tu edad: ");
+        int edad = teclado.nextInt();
 
-        // TODO: Evaluar con if / else si es mayor o menor de edad
-
+        
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad.");
+        } else {
+            System.out.println("Eres menor de edad.");
+        }
     }
 }
