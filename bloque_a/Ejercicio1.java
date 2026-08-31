37129239b32433d1d89507d750e4938b66d78ef9
Alesita-p <alejandrapsos24@gmail.com>
2026-08-31 14:58:12 -0500

    Update Ejercicio1.java

--- bloque_a/Ejercicio1.java
+++ bloque_a/Ejercicio1.java
@@ -1,13 +1,13 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        // TODO: Declara aquí tus variables y asígnales valores
+
         int edad = 17;
         double notaFinal = 9.5;
         char paralelo = 'A';
         boolean matriculado = true;
         String nombreEstudiante = "Alesita";
         
-        // TODO: Muestra los valores por consola
+
         System.out.println("Estudiante: " + nombreEstudiante);
         System.out.println("Edad: " + edad + " años");
         System.out.println("Nota Final: " + notaFinal);
