9d56d601e457e84d8381eb0d502810a4205f2b6d
Alesita-p <alejandrapsos24@gmail.com>
2026-08-31 14:59:07 -0500

    Clean up TODO comments in Ejercicio2.java
    Removed TODO comments for clarity and improved readability.

--- bloque_a/Ejercicio2.java
+++ bloque_a/Ejercicio2.java
@@ -2,16 +2,13 @@ import java.util.Scanner;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        // TODO: Instancia la clase Scanner
         Scanner teclado = new Scanner(System.in);
         
-        // TODO: Lee dos números por teclado
         System.out.print("Introduce el primer número: ");
         double num1 = teclado.nextDouble();
         
         System.out.print("Introduce el segundo número: ");
         double num2 = teclado.nextDouble();
-        // TODO: Calcula y muestra suma, resta, multiplicación y división
         double suma = num1 + num2;
         double resta = num1 - num2;
         double multiplicacion = num1 * num2;
